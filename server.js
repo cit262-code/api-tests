5b204917358bf5f2164a81e3dbaf76ae917955a2
Alejandro P <102633416+alejandroxp2022@users.noreply.github.com>
2023-06-21 04:56:21 -0400

    Update server.js to use my name

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express(); //app is a backend application
 
 app.get("/", (req,res)=>{
-    res.send("Welcome tp my Sean's backend application");
+    res.send("Welcome tp my Alejandro's backend application");
 }); // forward slash means the context root of the application, or the url without...
 
 app.get("/sean",(req,res)=>{
-    res.send("Hello Sean!");
+    res.send("Hello Alejandro!");
 })
 
 app.listen(3000,()=>{
